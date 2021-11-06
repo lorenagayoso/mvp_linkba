15484b8fe9ce2e9fb8f88b4ec4cdebf9656bb08a
lorenagayoso <lorena.gayoso.g@gmail.com>
2021-11-06 16:24:36 -0300

    mnn

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,13 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_6186d3af6885f60a50baa278';
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6186d3af6885f60a50baa278/1fjrb4e0h';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);})();
+
+
 (function($) {
   "use strict";
 
