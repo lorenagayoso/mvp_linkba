ca7937ea9aa0ca26206771310871c84dd1326783
lorenagayoso <lorena.gayoso.g@gmail.com>
2021-11-06 16:28:24 -0300

    esta es la buena

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,14 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_6186d3af6885f60a50baa278';
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/6186d3af6885f60a50baa278/1fjrb4e0h';
+s1.src='https://embed.tawk.to/6186d3af6885f60a50baa278/1fjrbof08';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);})();
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 
 
 (function($) {
